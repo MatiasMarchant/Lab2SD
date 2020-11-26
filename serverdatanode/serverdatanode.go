a4bcc54c8a7576a1aebcf28727c5a8c571035de7
NcolasDran <nduransepulveda@gmail.com>
2020-11-26 00:09:25 -0300

    test conexion maquinas

---
+++ serverdatanode/serverdatanode.go
@@ -0,0 +1,12 @@
+package serverdatanode
+
+import (
+	"golang.org/x/net/context"
+)
+
+type Server struct {
+}
+
+func (s *Server) EnvioMensajeTest(ctx context.Context, message *MensajeTest) (*MensajeTest, error) {
+	return message, nil
+}
