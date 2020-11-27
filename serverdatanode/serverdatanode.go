cb083bf3bd2943c89401cee62a3064a08c6f8213
NcolasDran <nduransepulveda@gmail.com>
2020-11-27 19:17:24 -0300

    test listado libros

--- serverdatanode/serverdatanode.go
+++ serverdatanode/serverdatanode.go
@@ -7,51 +7,12 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-
-	"bufio"
-	"strings"
-	"strconv"
 )
 
 type Server struct {
 }
 
-
-func listaDeLibros() string{
-	listado := ""
-	nLibro := 0
-	file, err := os.Open("log.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-		linea := scanner.Text()
-		p_linea := strings.Split(linea, " ")[0]
-		if _, err := strconv.Atoi(p_linea); err != nil {
-			nLibro += 1
-			n := strconv.Itoa(nLibro)
-			listado += n+" "+linea+"\n"
-		}		
-    }
-
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-	}
-	
-	return listado
-}
-
-
 func (s *Server) EnvioMensajeTest(ctx context.Context, message *MensajeTest) (*MensajeTest, error) {
-	if message.Mensaje == "listadoLibros\n" {
-		fmt.Printf("> Enviando listado de libros")
-		respuestaDataNode := listaDeLibros()
-		return &MensajeTest{Mensaje: respuestaDataNode}, nil
-	}
-	
 	//fmt.Printf("|Servidor| Se recibe: %s", message.Mensaje)
 	respuestaDataNode := "DataNode recibe: " + message.Mensaje
 	return &MensajeTest{Mensaje: respuestaDataNode}, nil
