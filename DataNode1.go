5a9ac2c59b75eaa6fbcc35a2b148471623c38d35
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-11-27 17:56:39 -0300

    B DN1 4

--- DataNode1.go
+++ DataNode1.go
@@ -120,6 +120,8 @@ func HacerPropuesta(metodo string, NombreLibroSubido string) {
 			log.Printf("err_files, no puede leer directorio: %v", err_files)
 		}
 		for indice_parte, f := range files {
+			fmt.Printf("Nombre scan: %s\n", f.Name())
+			fmt.Printf("NombreLibroSubido: %s\n", NombreLibroSubido)
 			if strings.Contains(f.Name(), NombreLibroSubido) {
 				Arreglo_indices_partes_libro = append(Arreglo_indices_partes_libro, strconv.Itoa(indice_parte))
 			}
