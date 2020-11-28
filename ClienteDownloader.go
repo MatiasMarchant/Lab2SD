a6eee77c8ac1b3f94b44ccba17ba8e6d2e2fa045
NcolasDran <nduransepulveda@gmail.com>
2020-11-27 23:53:07 -0300

    test obtener chunks libros

--- ClienteDownloader.go
+++ ClienteDownloader.go
@@ -76,8 +76,8 @@ func main() {
 			if err_NN != nil {
 				fmt.Print("Error al obtener listado: %s", err_NN)
 			} else {
-				fmt.Println(strings.Split(respuestaNN.Mensaje, "\n")[nLibro-1]) 
-				tituloLibro := strings.Split(strings.Split(respuestaNN.Mensaje, "\n")[nLibro-1], "\n")[1]
+
+				tituloLibro := strings.Split(strings.Split(respuestaNN.Mensaje, "\n")[nLibro-1], " ")[1]
 				
 				mensajeNN := servernamenode.MensajeTest{
 					Mensaje: "ubicacion "+tituloLibro,
